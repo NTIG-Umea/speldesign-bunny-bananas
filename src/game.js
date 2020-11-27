f184728bf7e7e657bde5bd2c82ad25082bcfc8c9
Gabriel <gabrieloberg3@gmail.com>
2020-11-27 09:53:43 +0100

    backup

--- src/game.js
+++ src/game.js
@@ -1,18 +1,4 @@
-<!doctype html> 
-<html lang="en"> 
-<head> 
-    <meta charset="UTF-8" />
-    <title>Making your first Phaser 3 Game - Part 8</title>
-    <script src="//cdn.jsdelivr.net/npm/phaser@3.11.0/dist/phaser.js"></script>
-    <style type="text/css">
-        body {
-            margin: 0;
-        }
-    </style>
-</head>
-<body>
-
-<script type="text/javascript">
+import Phaser from 'phaser';
 
     var config = {
         type: Phaser.AUTO,
@@ -137,7 +123,3 @@
         star.disableBody(true, true);
     }
 
-</script>
-
-</body>
-</html>
